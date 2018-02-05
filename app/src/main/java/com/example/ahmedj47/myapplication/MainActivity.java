561f64979f20e9e74f33fa20f1e0f90356d68430
Ahmed <ahmedj47@uwosh.edu>
2018-02-05 15:21:30 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ahmedj47/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ahmedj47.myapplication;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            setContentView(R.layout.activity_main);
+        }
+        else{
+            setContentView(R.layout.activity_main);
+        }
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+}
